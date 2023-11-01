c5897588d2365e5571d59f45d391fa9c4e949308
Mario Umaña <mburgosgit003@gmail.com>
2023-10-31 23:34:41 -0600

    unpacking data from db to create a plot in streamlit

--- web-scraping-temperature-plot/functions.py
+++ web-scraping-temperature-plot/functions.py
@@ -1,6 +1,10 @@
 import requests
 import selectorlib
 import datetime
+import time
+import sqlite3
+
+connection = sqlite3.connect("./temperatures.db")
 
 URL = "https://programmer100.pythonanywhere.com/"
 
@@ -15,19 +19,23 @@ def scrape(url):
 def extract_temperature(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["temperature"]
-    
+
     return value
 
 
-def write_temperature(date, temperature):
-    with open('./temperatures.csv', 'a') as file:
-        file.write(f"{date},{temperature}" + "\n")
-        
+def write_temperature(data):
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperatures VALUES (?,?)", data)
+    connection.commit()
+
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    temperature = extract_temperature(scraped)
-    date_and_time = datetime.datetime.now()
-    dt_string = date_and_time.strftime("%Y-%m-%d-%H-%M-%S")
-    write_temperature(dt_string, temperature)
-    
+    while True:
+        scraped = scrape(URL)
+        date_and_time = datetime.datetime.now()
+        dt_string = date_and_time.strftime("%Y-%m-%d-%H-%M-%S")
+        temperature = extract_temperature(scraped)
+        data = f"{dt_string},{temperature}".split(',')
+        
+        write_temperature(data)
+        time.sleep(10)
--- web-scraping-temperature-plot/main.py
+++ web-scraping-temperature-plot/main.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import plotly.express as px
-import pandas as pd
+import sqlite3
+
+connection = sqlite3.connect("./temperatures.db")
+cursor = connection.cursor()
+dates = list(cursor.execute("SELECT date FROM temperatures"))
+x_asis = [date[0] for date in dates]
+
+temperatures = list(cursor.execute("SELECT temperature FROM temperatures"))
+y_asis = [temperature[0] for temperature in temperatures]
 
 st.header("Web scraping temperatures plot!")
-df = pd.read_csv("./temperatures.csv")
-
-x_asis = df["date"].tolist()
-y_asis = df["temperature"].tolist()
 
 plot = px.line(x=x_asis, y=y_asis, labels={"x":"dates", "y": "temperatures"})
 st.plotly_chart(plot)
